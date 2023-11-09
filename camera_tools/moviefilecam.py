c793792987bc4182fd669ac513fad031e61ebb47
ElTinmar <martin.privat@hotmail.fr>
2023-11-09 09:48:29 +0100

    add file

---
+++ camera_tools/moviefilecam.py
@@ -0,0 +1,125 @@
+from camera_tools.camera import Camera
+from camera_tools.frame import BaseFrame, Frame
+import time
+import numpy as np
+from numpy.typing import NDArray, ArrayLike
+from typing import Optional, Tuple
+
+class MovieFileCam(Camera):
+    """
+    Reads video from file
+    """
+
+    def __init__(self, filename: str, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.img_count: int = 0
+        self.time_start: float = time.monotonic()
+        self.filename = filename
+
+    def start_acquisition(self) -> None:
+        self.index = 0
+        self.time_start = time.monotonic()
+
+    def stop_acquisition(self) -> None:
+        pass
+
+    def get_frame(self) -> Frame:
+
+        self.img_count += 1
+        timestamp = time.monotonic() - self.time_start
+        img = np.zeros(self.shape, dtype=self.dtype)
+        frame = BaseFrame(self.img_count, timestamp, img)
+        return frame
+    
+    def set_exposure(self, exp_time: float) -> None:
+        pass
+
+    def get_exposure(self) -> Optional[float]:
+        pass
+
+    def get_exposure_range(self) -> Optional[Tuple[float,float]]:
+        pass
+
+    def get_exposure_increment(self) -> Optional[float]:
+        pass
+
+    def set_framerate(self, fps: float) -> None:
+        pass
+    
+    def get_framerate(self) -> Optional[float]:
+        pass
+
+    def get_framerate_range(self) -> Optional[Tuple[float,float]]:
+        pass
+
+    def get_framerate_increment(self) -> Optional[float]:
+        pass
+
+    def set_gain(self, gain: float) -> None:
+        pass
+
+    def get_gain(self) -> Optional[float]:
+        pass
+
+    def get_gain_range(self) -> Optional[Tuple[float,float]]:
+        pass
+
+    def get_gain_increment(self) -> Optional[float]:
+        pass
+
+    def set_ROI(self, left: int, bottom: int, height: int, width: int) -> None:
+        pass
+
+    def get_ROI(self) -> Optional[Tuple[int,int,int,int]]:
+        pass
+
+    def set_offsetX(self, offsetX: int) -> None:
+        pass
+
+    def get_offsetX(self) -> Optional[int]:
+        pass
+
+    def get_offsetX_range(self) -> Optional[int]:
+        pass
+
+    def get_offsetX_increment(self) -> Optional[int]:
+        pass
+
+    def set_offsetY(self, offsetY: int) -> None:
+        pass
+
+    def get_offsetY(self) -> Optional[int]:
+        pass
+
+    def get_offsetY_range(self) -> Optional[int]:
+        pass
+
+    def get_offsetY_increment(self) -> Optional[int]:
+        pass
+
+    def set_width(self, width: int) -> None:
+        pass
+
+    def get_width(self) -> Optional[int]:
+        pass
+
+    def get_width_range(self) -> Optional[int]:
+        pass
+
+    def get_width_increment(self) -> Optional[int]:
+        pass 
+
+    def set_height(self, height) -> None:
+        pass
+    
+    def get_height(self) -> Optional[int]:
+        pass    
+    
+    def get_height_range(self) -> Optional[int]:
+        pass
+
+    def get_height_increment(self) -> Optional[int]:
+        pass 
+
