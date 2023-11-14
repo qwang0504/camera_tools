aa2abcb3de077662fdbfee8ac73fed95ccf6b1a5
ElTinMar <martin.privat@hotmail.fr>
2023-11-14 16:12:54 +0100

    update

--- camera_tools/calibration.py
+++ camera_tools/calibration.py
@@ -1,4 +1,4 @@
-from .camera import Camera
+from .camera import Camera, Frame
 from typing import Tuple
 from numpy.typing import NDArray
 import cv2
@@ -9,9 +9,7 @@ def get_camera_distortion(
         checkerboard_size: Tuple[int,int],
         checkerboard_corners_world_coordinates_mm: NDArray,
         num_images: int = 10
-    ):
-    # TODO type hint return values
-
+    ) -> Tuple[NDArray, NDArray, NDArray]:
     '''
     Take picture of a checkerboard pattern with known world coordinates, and 
     compute lens distortion + transformation.
@@ -35,15 +33,16 @@ def get_camera_distortion(
 
     cam.stop_acquisition()
 
+    shp = frame.image.shape[:2] 
+
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
         world_coords, 
         image_coords, 
-        frame.image.shape[::-1], 
+        shp[::-1], 
         None, 
         None
     )
 
-    shp = frame.image.shape[:2]
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, shp, 1, shp)
 
     return mtx, newcameramtx, dist
@@ -51,11 +50,12 @@ def get_camera_distortion(
 def get_checkerboard_corners(
         cam: Camera,
         checkerboard_size: Tuple[int,int],
-    ): 
+    ) -> Tuple[Frame, NDArray]: 
     # TODO type hint return values
     '''
     take a picture every one second and tries to find checkerboard corners
     '''
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     checkerboard_found = False
     cv2.namedWindow('camera')
@@ -64,22 +64,29 @@ def get_checkerboard_corners(
         frame = cam.get_frame()
         
         # display image, detect corners if y is pressed
-        cv2.display('camera', frame.image)
+        cv2.imshow('camera', frame.image)
         key = cv2.waitKey(33)
 
         if key == ord('y'):
-            checkerboard_found, corners = cv2.findChessboardCornersSB(frame.image, checkerboard_size)
-            
+
+            checkerboard_found, corners = cv2.findChessboardCorners(frame.image, checkerboard_size)
+
             if checkerboard_found:
 
+                corners_sub = cv2.cornerSubPix(frame.image[:,:,1], corners, (11,11), (-1,-1), criteria)
+
                 # show corners
-                cv2.drawChessboardCorners(frame.image, checkerboard_size, corners, checkerboard_found)
+                cv2.drawChessboardCorners(frame.image, checkerboard_size, corners_sub, checkerboard_found)
                 cv2.imshow('chessboard', frame.image)
                 key = cv2.waitKey(0)
 
                 # return images and detected corner if y is pressed
                 if key == ord('y'):
+                    cv2.destroyAllWindows()
                     return frame, corners
+            
+            else:
+                print('checkerboard not found')
 
 def get_camera_px_per_mm(
         cam: Camera,
@@ -94,4 +101,5 @@ def get_camera_px_per_mm(
     frame, corners_px = get_checkerboard_corners(cam, checkerboard_size)
     cam.stop_acquisition()
 
+    cv2.und
     solve(corners_px, checkerboard_corners_world_coordinates_mm)
