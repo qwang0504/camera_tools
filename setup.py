075f509e2a1cae65f1cf85acc70794de597008a7
Tinmar <ElTinmar@users.noreply.github.com>
2023-12-13 11:20:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='camera_tools',
-    python_requires='==3.10',
+    python_requires='==3.9',
     author='Martin Privat',
     version='0.1.4',
     packages=['camera_tools','camera_tools.tests'],
@@ -11,7 +11,7 @@ setup(
     long_description=open('README.md').read(),
     install_requires=[
         "numpy", 
-        "opencv-contrib-python-rolling @ https://github.com/ElTinmar/build_opencv/raw/main/opencv_contrib_python_rolling-4.8.0.20231212-cp310-cp310-linux_x86_64.whl",
+        "opencv-contrib-python-rolling @ https://github.com/ElTinmar/build_opencv/raw/main/opencv_contrib_python_rolling-4.8.0.20231212-cp39-cp39-linux_x86_64.whl",
         "harvesters",
         "v4l2py"
     ]
