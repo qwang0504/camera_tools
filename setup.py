d4f4f452ba00338c3856e6e45a72fdfbfa370413
Tinmar <ElTinmar@users.noreply.github.com>
2023-12-17 21:27:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=open('README.md').read(),
     install_requires=[
         "numpy", 
-        "opencv-contrib-python-rolling @ https://github.com/ElTinmar/build_opencv/raw/main/opencv_contrib_python_rolling-4.8.0.20231212-cp39-cp39-linux_x86_64.whl",
+        "opencv-contrib-python",
         "harvesters",
         "v4l2py"
     ]
