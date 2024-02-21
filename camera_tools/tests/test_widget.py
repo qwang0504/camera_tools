b2bee215d41732863b0280a5ca28326fd2211c4a
ElTinmar <martin.privat@hotmail.fr>
2024-02-21 13:21:08 +0100

    create a window

--- camera_tools/tests/test_widget.py
+++ camera_tools/tests/test_widget.py
@@ -1,19 +1,26 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from camera_tools import (
-    CameraWidget, XimeaCamera
+    CameraWidget, XimeaCamera, Camera
 )
 
 #cam = MovieFileCam('/home/martin/Downloads/19-40-44.avi')
 #cam = OpenCV_Webcam()
 #cam = V4L2_Webcam()
-cam = XimeaCamera()
+
 #cam = RandomCam(shape=(512,512), dtype=np.uint8)
 
+class MainWindow(QMainWindow):
+    
+    def __init__(self, camera: Camera, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.camera_widget = CameraWidget(camera)
+        self.setCentralWidget(self.camera_widget)
 
-# test widget
-app = QApplication(sys.argv)
-window = CameraWidget(cam)
-window.show()
-sys.exit(app.exec())
+if __name__ == '__main__':
+    cam = XimeaCamera()
+    app = QApplication(sys.argv)
+    window = MainWindow(cam)
+    window.show()
+    sys.exit(app.exec())
 
