66796f5580dfd80d701f17c312fd66d61827b956
ElTinmar <martin.privat@hotmail.fr>
2024-02-21 11:29:57 +0100

    try

--- camera_tools/__init__.py
+++ camera_tools/__init__.py
@@ -4,11 +4,31 @@ from .camera_widget import *
 from .randomcam import *
 from .zerocam import *
 from .frame import *
-from .webcam import *
-from .webcam_v4l2 import *
-from .moviefilecam import *
-from .ring_buffer import *
-from .calibration import get_camera_distortion, get_camera_px_per_mm
+
+try:
+    from .webcam import *
+except ModuleNotFoundError:
+    print('webcam not available')
+
+try:
+    from .webcam_v4l2 import *
+except ModuleNotFoundError:
+    print('v4l2 not available')
+
+try:
+    from .moviefilecam import *
+except ModuleNotFoundError:
+    print('moviefilecam not available')
+
+try:
+    from .ring_buffer import *
+except ModuleNotFoundError:
+    print('ring buffer not available')
+
+try:
+    from .calibration import get_camera_distortion, get_camera_px_per_mm
+except ModuleNotFoundError:
+    print('calibration not available')
 
 try:
     from .genicam import *
