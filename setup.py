c757ac702e71b4d03d0e66a5efdd0a7d526e5608
ElTinmar <martin.privat@hotmail.fr>
2024-03-01 21:28:59 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name='camera_tools',
     python_requires='>=3.8',
     author='Martin Privat',
-    version='0.1.4',
+    version='0.1.5',
     packages=['camera_tools','camera_tools.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='camera tools',
     long_description=open('README.md').read(),
     install_requires=[
         "numpy", 
+        "PyQt5",
         "opencv-python",
         "harvesters",
         "v4l2py"
