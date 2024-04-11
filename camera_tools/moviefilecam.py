ff8e40f82f78233ed5b45a6d6159fd0aaf22d5b2
ElTinmar <martin.privat@hotmail.fr>
2024-04-11 10:43:08 +0200

    close instead of release

--- camera_tools/moviefilecam.py
+++ camera_tools/moviefilecam.py
@@ -29,7 +29,7 @@ class MovieFileCam(Camera):
         self.reader = cv2.VideoCapture(self.filename)
 
     def stop_acquisition(self) -> None:
-        self.reader.release()
+        self.reader.close()
 
     def get_frame(self) -> Optional[Frame]:
         if self.reader is not None:
