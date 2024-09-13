33a097c4a4c3ba1efd966317eef6966eb985447e
ElTinmar <martin.privat@hotmail.fr>
2024-09-14 00:21:32 +0200

    higher fps

--- camera_tools/moviefilecam.py
+++ camera_tools/moviefilecam.py
@@ -77,7 +77,7 @@ class MovieFileCam(Camera):
         return self.fps
 
     def get_framerate_range(self) -> Optional[Tuple[float,float]]:
-        return (1,60)
+        return (1,240)
 
     def get_framerate_increment(self) -> Optional[float]:
         return 1
@@ -263,7 +263,7 @@ class BufferedMovieFileCam(Camera):
         return self.fps
     
     def get_framerate_range(self) -> Optional[Tuple[float,float]]:
-        return (1,60)
+        return (1,240)
 
     def get_framerate_increment(self) -> Optional[float]:
         return 1
